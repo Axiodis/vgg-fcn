71ef3ef3772276c7d116b0959ec00dc0860c1777
Martin Alexandru <alex.martin96@gmail.com>
2018-03-04 11:54:42 +0200

    First

---
+++ train.py
@@ -0,0 +1,130 @@
+from vgg16_fcn import VGG16_FCN
+from datetime import datetime
+from data_reader import DataReader
+import tensorflow as tf
+import os
+import numpy as np
+import logging
+
+FLAGS = tf.app.flags.FLAGS
+flags.DEFINE_string('main_dir', 'main', 'Main Directory.')
+
+log_file = "{}.log".format(datetime.now().strftime("%d-%m-%Y"))
+logging.basicConfig(filename = log_file, format='%(levelname)s (%(asctime)s): %(message)s', level = logging.INFO)
+
+num_epochs = 1000
+NUM_CLASSES = 20
+learning_rate = 1e-6
+batch_size = 2
+
+images_dir = os.path.join(FLAGS.main_dir,"/VOC2012/JPEGImages")
+labels_dir = os.path.join(FLAGS.main_dir,"/VOC2012/SegmentationClass")
+
+filewriter_path = os.path.join(FLAGS.main_dir,"/vgg_fcn/tensorboard")
+checkpoint_path = os.path.join(FLAGS.main_dir,"/vgg_fcn/checkpoints")
+
+tf.reset_default_graph()
+
+# TF placeholder for graph input and output
+x = tf.placeholder(tf.float32, shape=[batch_size, None, None, 3], name="input_image")
+y = tf.placeholder(tf.int32, shape=[batch_size, None, None], name="input_label")
+keep_prob = tf.placeholder(tf.float32)
+
+"""Build Model"""
+print("[MODEL] => Time: {} Building".format(datetime.now()))
+model = VGG16_FCN(x, NUM_CLASSES, keep_prob)
+logging.info("Model build")
+
+
+"""Define loss function"""
+loss = tf.reduce_mean((tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = model.upscore8, name="Loss")))
+
+
+"""Define training op"""
+trainable_var = tf.trainable_variables() # Collect all trainable variables for the net
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+
+grads = optimizer.compute_gradients(loss, var_list=trainable_var)
+
+train_op = optimizer.apply_gradients(grads)
+
+# Initialize the FileWriter
+writer = tf.summary.FileWriter(filewriter_path)
+
+# Initialize saver for store model checkpoints
+saver = tf.train.Saver()
+
+#Initialize data reader
+train_generator = DataReader(images_dir,labels_dir,batch_size)
+train_batches_per_epoch = np.floor(train_generator.data_size / batch_size).astype(np.int16)
+
+
+
+"""Start Tensorflow session"""
+print("[TRAIN] => Time: {} Start session".format(datetime.now()))
+logging.info("Session started")
+
+try:
+    with tf.Session() as sess:
+     
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+      
+        # Add the model graph to TensorBoard
+        writer.add_graph(sess.graph)
+      
+        # Load the pretrained weights into the non-trainable layer
+        model.load_initial_weights(sess,os.path.join(FLAGS.main_dir,"/vgg_fcn/vgg16.npy"))
+      
+        print("[TRAIN] => Train batches per epoch: {}".format(train_batches_per_epoch))
+        print("[TRAIN] => Time: {} Start training...".format(datetime.now()))
+        print("[TENSORBOARD] => Open Tensorboard at --logdir {}".format(filewriter_path))
+        logging.info("Training started")
+    
+        for epoch in range(num_epochs):
+        
+            print("[EPOCH] => Time: {} Epoch number: {}".format(datetime.now(), epoch+1))
+            logging.info("Epoch: {}".format(epoch+1))
+            
+            step = 1
+            
+            while step < train_batches_per_epoch:
+                
+                print("Step {} of {}".format(step, train_batches_per_epoch))
+                
+                with tf.device('/cpu:0'):
+                    batch_xs, batch_ys = train_generator.next_batch()
+                
+                sess.run(train_op, feed_dict={x: batch_xs, 
+                                              y: batch_ys, 
+                                              keep_prob: 0.5})
+                step += 1
+                
+            train_generator.reset_pointer()
+            
+            if(epoch % 25 == 0 and epoch > 0):
+            
+                print("[SAVE] => Time: {} Saving checkpoint of model...".format(datetime.now()))  
+                
+                checkpoint_name = os.path.join(checkpoint_path, 'model_epoch'+str(datetime.now())+'.ckpt')
+                save_path = saver.save(sess, checkpoint_name)  
+                
+                print("[SAVE] => Time: {} Model checkpoint saved at {}".format(datetime.now(), checkpoint_name))
+                
+        print("[TRAIN] => Time: {} Finish training...".format(datetime.now()))
+        logging.info("Training finished")
+    
+        checkpoint_name = os.path.join(checkpoint_path, 'final_model'+str(datetime.now())+'.ckpt')
+        print("[FINAL-SAVE] => Time: {} Final model checkpoint saved at {}".format(datetime.now(), checkpoint_name))
+                
+except Exception as e:
+    print("[ERROR] => Time: {} Unexpected error encountered. Please check the log file.".format(datetime.now()))
+    logging.error("Error message: {}".format(e))
+    logging.info("Terminating...".format(e))
+        
+    
+    
+    
+    
+    
---
+++ vgg16_fcn.py
@@ -0,0 +1,237 @@
+import numpy as np
+import tensorflow as tf
+import inspect
+import os
+from math import ceil
+
+VGG_MEAN = [103.939, 116.779, 123.68]# Mean value of pixels in R G and B channels
+
+
+class VGG16_FCN:
+    def __init__(self, x, num_classes, keep_prob, vgg16_npy_path=None):
+        
+        self.X = x
+        self.NUM_CLASSES = num_classes
+        self.KEEP_PROB = keep_prob
+        
+        self.SKIP_LAYERS = ['fc8']
+        
+        self.build()
+        
+    def build(self):
+        
+        # Convert RGB to BGR and substract pixels mean
+        red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=self.X)
+
+        x = tf.concat(axis=3, values=[
+            blue - VGG_MEAN[0],
+            green - VGG_MEAN[1],
+            red - VGG_MEAN[2],
+        ])
+    
+        #------------Build VGG16-FCN normal layers--------------------------
+        
+        #Layer 1
+        self.conv1_1 = self.conv(x, 3, 3, 64, 1, 1, "conv1_1")
+        self.conv1_2 = self.conv(self.conv1_1, 3, 3, 64, 1, 1, "conv1_2")
+        self.pool1 = self.max_pool(self.conv1_2, 2, 2, 2, 2, 'pool1')
+        
+        #Layer 2
+        self.conv2_1 = self.conv(self.pool1, 3, 3, 128, 1, 1, "conv2_1")
+        self.conv2_2 = self.conv(self.conv2_1, 3, 3, 128, 1, 1, "conv2_2")
+        self.pool2 = self.max_pool(self.conv2_2, 2, 2, 2, 2, 'pool2')
+        
+        #Layer 3
+        self.conv3_1 = self.conv(self.pool2, 3, 3, 256, 1, 1, "conv3_1")
+        self.conv3_2 = self.conv(self.conv3_1, 3, 3, 256, 1, 1, "conv3_2")
+        self.conv3_3 = self.conv(self.conv3_2, 3, 3, 256, 1, 1, "conv3_3")
+        self.pool3 = self.max_pool(self.conv3_3, 2, 2, 2, 2, 'pool3')
+        
+        #Layer 4
+        self.conv4_1 = self.conv(self.pool3, 3, 3, 512, 1, 1, "conv4_1")
+        self.conv4_2 = self.conv(self.conv4_1, 3, 3, 512, 1, 1, "conv4_2")
+        self.conv4_3 = self.conv(self.conv4_2, 3, 3, 512, 1, 1, "conv4_3")
+        self.pool4 = self.max_pool(self.conv4_3, 2, 2, 2, 2, 'pool4')
+        
+        #Layer 5
+        self.conv5_1 = self.conv(self.pool4, 3, 3, 512, 1, 1, "conv5_1")
+        self.conv5_2 = self.conv(self.conv5_1, 3, 3, 512, 1, 1, "conv5_2")
+        self.conv5_3 = self.conv(self.conv5_2, 3, 3, 512, 1, 1, "conv5_3")
+        self.pool5 = self.max_pool(self.conv5_3, 2, 2, 2, 2, 'pool5')
+    
+        #-----------------------Build VGG16-FCN fully connvolutional layers---------
+        
+        self.conv6 = self.conv(self.pool5, 7, 7, 4096, 1, 1, "fc6")
+        self.drop6 = self.dropout(self.conv6, self.KEEP_PROB)
+        
+        self.conv7 = self.conv(self.drop6, 1, 1, 4096, 1, 1, "fc7")
+        self.drop7 = self.dropout(self.conv7, self.KEEP_PROB)
+        
+        self.score_fr = self.conv(self.drop7, 1, 1, self.NUM_CLASSES, 1, 1, "fc8", 
+                                  w_stddev = (2 / self.drop7.get_shape()[3].value)**0.5, 
+                                  b_constant = 0.0, relu = False)
+        
+        self.pred = tf.argmax(self.score_fr, dimension=3)
+        
+        
+        #-----------------------Build VGG16-FCN upsample layers---------
+        
+        self.upscore2 = self.deconv(self.score_fr,
+                                            shape=tf.shape(self.pool4),
+                                            num_classes = self.NUM_CLASSES,
+                                            name='upscore2',
+                                            ksize=4, stride=2)
+        self.score_pool4 = self.conv(self.pool4, 1, 1, self.NUM_CLASSES, 1, 1, "score_pool4", 
+                                     w_stddev = 0.001, 
+                                     b_constant = 0.0, relu = False)
+        self.fuse_pool4 = tf.add(self.upscore2, self.score_pool4)
+
+        self.upscore4 = self.deconv(self.fuse_pool4,
+                                            shape=tf.shape(self.pool3),
+                                            num_classes = self.NUM_CLASSES,
+                                            name='upscore4',
+                                            ksize=4, stride=2)
+        self.score_pool3 = self.conv(self.pool3, 1, 1, self.NUM_CLASSES, 1, 1, "score_pool3", 
+                                     w_stddev = 0.0001, 
+                                     b_constant = 0.0, relu = False)
+        self.fuse_pool3 = tf.add(self.upscore4, self.score_pool3)
+
+        self.upscore8 = self.deconv(self.fuse_pool3,
+                                             shape=tf.shape(x),
+                                             num_classes = self.NUM_CLASSES,
+                                             name='upscore8',
+                                             ksize=16, stride=8)
+
+        self.pred_up = tf.argmax(self.upscore8, dimension=3)
+        
+        
+        
+    def deconv(self, x, shape, num_classes, name, ksize=4, stride=2):
+        
+        strides = [1, stride, stride, 1]
+        
+        with tf.variable_scope(name):
+            in_features = x.get_shape()[3].value
+
+            if shape is None:
+                in_shape = tf.shape(x)
+
+                h = ((in_shape[1] - 1) * stride) + 1
+                w = ((in_shape[2] - 1) * stride) + 1
+                new_shape = [in_shape[0], h, w, num_classes]
+            else:
+                new_shape = [shape[0], shape[1], shape[2], num_classes]
+                
+            output_shape = tf.stack(new_shape)
+
+            f_shape = [ksize, ksize, num_classes, in_features]
+            weights = self.get_deconv_filter(f_shape)
+            
+            deconv = tf.nn.conv2d_transpose(x, weights, output_shape, strides=strides, padding='SAME')
+
+        return deconv
+
+    def get_deconv_filter(self, f_shape):
+        width = f_shape[0]
+        height = f_shape[1]
+        f = ceil(width/2.0)
+        c = (2 * f - 1 - f % 2) / (2.0 * f)
+        bilinear = np.zeros([f_shape[0], f_shape[1]])
+        for x in range(width):
+            for y in range(height):
+                value = (1 - abs(x / f - c)) * (1 - abs(y / f - c))
+                bilinear[x, y] = value
+        weights = np.zeros(f_shape)
+        for i in range(f_shape[2]):
+            weights[:, :, i, i] = bilinear
+
+        init = tf.constant_initializer(value=weights, dtype=tf.float32)
+        var = tf.get_variable(name="up_filter", initializer=init, shape=weights.shape)
+        return var
+        
+    def conv(self, x, filter_height, filter_width, num_filters, stride_y, stride_x, name, 
+             w_stddev = None, b_constant = None, relu = True, padding='SAME'):
+        
+        input_channels = int(x.get_shape()[-1])
+        w_initializer = None
+        b_initializer = None
+        
+        if w_stddev is not None:
+            w_initializer = tf.truncated_normal_initializer(stddev=w_stddev)
+    
+        if b_constant is not None:
+            b_initializer = tf.constant_initializer(b_constant)
+    
+        with tf.variable_scope(name, reuse = tf.AUTO_REUSE) as scope:
+            
+            
+            
+            weights = tf.get_variable('weights', shape=[filter_height,
+                                                        filter_width,
+                                                        input_channels,
+                                                        num_filters],
+                                                initializer=w_initializer)
+    
+            biases = tf.get_variable('biases', shape=[num_filters], initializer = b_initializer)
+    
+        
+        conv = tf.nn.conv2d(x, weights, strides=[1, stride_y, stride_x, 1], padding=padding)
+    
+        bias = tf.nn.bias_add(conv, biases)
+        
+        if relu:
+            relu = tf.nn.relu(bias, name=scope.name)
+
+            return relu
+        
+        return bias
+    
+    def max_pool(self, x, filter_height, filter_width, stride_y, stride_x, name, padding='SAME'):
+        
+        return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+                              strides=[1, stride_y, stride_x, 1],
+                              padding=padding, name=name)
+        
+    def dropout(self, x, keep_prob):
+        return tf.nn.dropout(x, keep_prob)
+
+    def load_initial_weights(self, session, vgg16_npy_path=None):
+        
+        if vgg16_npy_path is None:
+             path = inspect.getfile(VGG16_FCN)
+             path = os.path.abspath(os.path.join(path, os.pardir))
+             path = os.path.join(path, "vgg16.npy")
+             vgg16_npy_path = path
+
+        weights_dict = np.load(vgg16_npy_path, encoding='latin1').item()
+        
+        for op_name in weights_dict:
+            
+            if op_name not in self.SKIP_LAYERS:
+    
+                with tf.variable_scope(op_name, reuse = tf.AUTO_REUSE):
+                    
+                    """Need to reshape fc layers weights"""
+                    if(op_name == 'fc6' or op_name == 'fc7'):
+                        var = tf.get_variable('weights')
+                        shape = var.get_shape();
+                        
+                        weights = weights_dict[op_name][0]
+                        weights = weights.reshape(shape)
+                        
+                        init = tf.constant_initializer(value=weights, dtype=tf.float32)
+                        weights = tf.get_variable(name="weights", initializer=init, shape=shape)
+                        session.run(var.assign(weights))
+                        
+                        var2 = tf.get_variable('biases')
+                        session.run(var2.assign(weights_dict[op_name][1]))
+                    else:
+                        for data in weights_dict[op_name]:
+        
+                            if len(data.shape) == 1:
+                                var = tf.get_variable('biases')
+                                session.run(var.assign(data))
+        
+                            else:
+                                var = tf.get_variable('weights')
+                                session.run(var.assign(data))
